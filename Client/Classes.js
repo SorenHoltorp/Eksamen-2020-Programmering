807c0a1d28a5fb5721cf181fe42c5e5754927f38
Søren Holtorp <SorenHoltorp@Administrators-MacBook-Air-86.local>
2020-11-22 14:22:53 +0100

    har lavet first forsøg på classes

---
+++ Client/Classes.js
@@ -0,0 +1,36 @@
+class User{
+
+}
+
+class Login{
+
+}
+
+class Create_acc{
+
+}
+
+class like{
+
+
+}
+
+class Match{
+
+}
+
+class MatchOverview{
+
+}
+
+class Potential{
+    
+}
+
+class Chat{
+
+}
+
+class UserNotifications{
+
+}
