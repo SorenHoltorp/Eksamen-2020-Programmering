77d0258b08a738184263d4717efc61b11928e85c
Søren Holtorp <SorenHoltorp@Administrators-MacBook-Air-86.local>
2020-11-26 08:05:22 +0100

    Lavet ny simpel server og app filer

--- Server/app.js
+++ Server/app.js
@@ -1,47 +1,40 @@
-const http = require("http");
+
 //importere express
 const express = require("express");
 //importere cors, en nødvendighed for at bruge http
-const cors = require("cors");
+//const cors = require("cors");
 // importere body-parser - omskriver json
-const bodyParser = require("body-parser")
+//const bodyParser = require("body-parser")
 // bruger express til vores server
 const app = express();
 //importere mongoose, et udvidelse til MongoDb
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 //nodejs måde at logge ting på - middleware
-const morgan = require("morgan");
+//const morgan = require("morgan");
 
 
 
-
-
-
-
-//Sætter porten til et vilkårligt tal
-port = process.env.PORT || 30000;
 
 //Henter mongoDb stien til databasen
-require("dotenv/config");
+//require("dotenv/config");
 
 // Så jeg kan snakke med Json filen ved alle request
-app.use(cors())
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(morgan("dev"));
+//app.use(cors())
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: false}));
+//app.use(morgan("dev"));
+
+//middleware, en request skal gå igennem app eller hvad vi sætter ind i parameteren
+app.use((req,res,next) => {
+    res.status(200).json({
+        message: "Du har connection til serveren"
+    });
+});
 
 //Import route
 
 
 
-
-
-
-
-
-// Vi sætter appen igang med at lytte til serveren
-// På den port som blev defineret øverst.
-app.listen(port);
 
 // Nu kører serveren ved npm start i consollen. 
 module.exports = app;
---
+++ Server/server.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const app = require("./app");
+
+//Sætter porten til et vilkårligt tal OR 30000
+const port = process.env.PORT || 3000;
+
+
+//opretter serveren
+const server = http.createServer(app);
+
+//Serveren lytter
+// Vi sætter appen igang med at lytte til serveren
+// På den port som blev defineret øverst.
+
+server.listen(port);
